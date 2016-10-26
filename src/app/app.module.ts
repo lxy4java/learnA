f50ee15078ab37cb41ef56580bfe4b3d24430425
lixy <lixiaoyao@zszq.com>
2016-10-26 10:54:44 +0800

    hello world

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,27 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { HelloComponent } from './hello/hello.component';
+/***
+ * 
+ * component  不要放入 import 里面 
+ * 不然会出以下错误：
+ * 
+ * metadata_resolver.js:230Uncaught Error: Unexpected directive 'HelloComponent' imported by the module 'AppModule'
+ */
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HelloComponent]
 })
 export class AppModule { }
