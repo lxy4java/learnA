497924812f6b3b6a5fa63ae4e6e0386c84761a04
lixy <lixiaoyao@zszq.com>
2016-11-03 11:13:57 +0800

    router III  路由传参

--- src/app/router/cpp.component.ts
+++ src/app/router/cpp.component.ts
@@ -1,11 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'cpp',
-    template: '<h1>哈？听说go要超过我。</h1>'
+    template: '<h1>哈？听说{{name}}要超过我。</h1>'
 })
-export class CppComponent implements OnInit {
-    constructor() { }
+export class CppComponent implements OnInit, OnDestroy {
+    constructor(private route: ActivatedRoute) { }
+    name = 'go'
+    ngOnInit() {
+       /***  console.log(this.route.params.subscribe(params=>{
+            console.log(params);
+        }))
+        ***/
+     console.log(this.route.snapshot.params['key']);
+     this.name= this.route.snapshot.params['key'];
+    }
 
-    ngOnInit() { }
+    ngOnDestroy() {
+
+    }
 }
--- src/app/router/go.component.ts
+++ src/app/router/go.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Router}             from '@angular/router';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,13 +8,13 @@ import {Router}             from '@angular/router';
     template: '<h1>我要超越{{name}}</h1><button (click)="go()">点击</button>'
 })
 export class GoComponent implements OnInit {
-    constructor(private router: Router,) { }
+    constructor(private router: Router, ) { }
 
     ngOnInit() { }
-    name ='cpp'
-    go(){
+    name = 'cpp'
+    go() {
         console.log('跳转到cpp');
-       this.router.navigate(['cpp']);
+        this.router.navigate(['cpp',{ key: "go" }]);
     }
 }
 /******方法中调用router 需要在构造器中引入，但不需要 Inject */
