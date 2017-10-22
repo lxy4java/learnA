654174afdec74f42afb757fb4ba72601c147273e
lixy <lxy4java@gmail.com>
2017-10-22 10:36:22 +0900

    升级下版本，没有优化最小依赖

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -3,10 +3,12 @@ import { Component, OnInit } from '@angular/core';
 /***用cli 创建 component , 这里代码简单没用 模版  */
 @Component({
     selector: 'hello',
-    template: '<h1>hello world</h1>'
+    template: '<input type ="text" [(ngModel)]="title"><span>{{title}}</span>'
 })
 export class HelloComponent implements OnInit {
+    
+    title='hello';
     constructor() { }
-
+    
     ngOnInit() { }
 }
