e8b69bb32eb43c99be5604237d58dacd9075d0a3
lixy <lxy4java@gmail.com>
2017-11-02 16:01:33 +0900

    - -

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+
+import { HelloComponent } from './shared/hello.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HelloComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +16,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HelloComponent]
 })
 export class AppModule { }
