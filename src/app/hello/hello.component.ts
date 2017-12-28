11ee0fd21ff8161384da35ceba8bb68f5405c8a6
lixy <lxy4java@gmail.com>
2017-12-28 21:18:54 +0900

    - - 测试 ngstyle

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -3,12 +3,30 @@ import { Component, OnInit } from '@angular/core';
 /***用cli 创建 component , 这里代码简单没用 模版  */
 @Component({
     selector: 'hello',
-    template: '<input type ="text" [(ngModel)]="title"><span>{{title}}</span>'
+    templateUrl: 'hello.component.html'
 })
 export class HelloComponent implements OnInit {
     
     title='hello';
     constructor() { }
-    
+    dsp='inline';
+    show={
+        'display':this.dsp
+    }
     ngOnInit() { }
+    change(){
+        if(this.dsp==='inline'){
+            this.dsp='none';
+            this.show={
+                'display': 'none'
+            }
+        }else{
+            this.dsp='inline'
+            this.show={
+                'display': 'inline'
+            }
+        }
+        console.log(this.dsp)
+        console.log(this.show)
+    }
 }
